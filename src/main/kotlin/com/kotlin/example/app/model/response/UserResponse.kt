63238be2bceb130669c6c94204adab92456f9a8b
Leonardo Gois Hengler Lopes <henglerleo@hotmail.com>
2022-06-21 15:26:28 -0300

    Update to tropikalia aws v5

--- src/main/kotlin/com/kotlin/example/app/model/response/UserResponse.kt
+++ src/main/kotlin/com/kotlin/example/app/model/response/UserResponse.kt
@@ -3,6 +3,4 @@ package com.kotlin.example.app.model.response
 data class UserResponse(
     val name: String,
     val age: Int,
-    val doc: Long,
-    val test: String = "Testing"
-)
+    val doc: Long
